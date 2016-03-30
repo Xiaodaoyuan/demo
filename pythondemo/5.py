e3463a29eed6347ea160e1e2773a3d1f931a25e0
xiaodaoyuan <xiaodaoyuan@mofanghr.com>
2016-03-30 10:00:29 +0800

    modify 5.py

--- pythondemo/5.py
+++ pythondemo/5.py
@@ -21,18 +21,28 @@ class BDTB:
                 return None
      
     def getTitle(self,content):
-        print content
         pattern = re.compile('<title>(.*?)</title>',re.S)
         result = re.search(pattern,content)
-        print result
         if result:
            print result.group(1)
            return result.group(1)
         else:
            print 'None title'
            return None
+ 
+    def getContent(self,content):
+        pattern = re.compile('<div id="post_content_.*?" class="d_post_content j_d_post_content ">(.*?)</div>')
+        result = re.findall(pattern,content)
+        if result:
+           for m in result:
+               print m
+           return result
+        else:
+           print 'None content'
+           return None
 
 baseurl = 'http://tieba.baidu.com/p/3138733512' 
 bdtb = BDTB(baseurl,1)
 content = bdtb.getPage(1)
 title = bdtb.getTitle(str(content))
+contentdict=bdtb.getContent(str(content))
